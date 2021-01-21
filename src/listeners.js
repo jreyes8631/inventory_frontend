4e4d7b735c2b5fc5d8af2f7248dd92edc43b3b1a
jreyes8631 <jreyes8631@gmail.com>
2021-01-20 19:41:10 -0500

    add event to submit item

--- src/listeners.js
+++ src/listeners.js
@@ -7,3 +7,17 @@ document.addEventListener('DOMContentLoaded', function(e){
  Category.all();
  Item.all();
 })
+
+document.addEventListener('submit', function(e){
+    let target = e.target;
+    if(target.matches('#newItem')){
+     e.preventDefault();
+     let formData = {}
+     target.querySelectorAll('input').forEach(function(input){
+       formData[input.name] = input.value;
+     })
+     Item.create(formData)
+     target.reset();
+    
+    }
+})
