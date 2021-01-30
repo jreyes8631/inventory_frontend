27bc9898a66f6d48c92ee2f0a7284de880fc3b4c
jreyes8631 <jreyes8631@gmail.com>
2021-01-30 17:57:59 -0500

    add category select option

--- src/category.js
+++ src/category.js
@@ -4,38 +4,38 @@ class Category {
     whitelist.forEach(attr => this[attr] = attributes[attr])
   }
 
-    static container(){
-      return this.c ||= document.querySelector("#categories-list")
-    }
-   
+  static container(){
+    return this.c ||= document.querySelector("#categories-list")
+  }
+  
 
-    static all(){
-      return fetch("http://localhost:3000/categories", {
-        headers:{
-         "Accept": "application/json",
-         "Content-Type": "application/json"
-        }
-      })
-      .then(res => {
-        if(res.ok){
-          return res.json()
-        } else{
-          return res.text().then(error => Promise.reject(error))
-        }
-      })
-      .then(categoryArray => {
-        this.collection = categoryArray.map(attrs => new Category(attrs))
-        let categoryList = this.collection.map(c => c.render())
-        this.container().append(...categoryList)
-        return this.collection
-      })
-    }
+  static all(){
+    return fetch("http://localhost:3000/categories", {
+      headers:{
+        "Accept": "application/json",
+        "Content-Type": "application/json"
+      }
+    })
+    .then(res => {
+      if(res.ok){
+        return res.json()
+      } else{
+        return res.text().then(error => Promise.reject(error))
+      }
+    })
+    .then(categoryArray => {
+      this.collection = categoryArray.map(attrs => new Category(attrs))
+      let categoryList = this.collection.map(c => c.render())
+      this.container().append(...categoryList)
+      return this.collection
+    })
+  }
 
   
   render() {
  
-  this.element ||= document.createElement('select');
-   this.element.class = "container";
+   this.element ||= document.getElementById('categories-Select');
+   this.element.classList.add(..."px-6 py-3 text-left text-xs font-medium text-black-500 uppercase tracking-wider bg-green-600".split(" "));
 
    this.nameOp  ||= document.createElement('option');
    this.nameOp.class = "container-category";
@@ -46,5 +46,6 @@ class Category {
    return this.element;
   }
 
+  
 
 }
